00eabd42936f75ad4ef99d448a2da8213cdf9fce
Phil Yoo <philyoo@Phils-MacBook-Air.local>
2018-06-14 17:47:33 -0400

    stored game data virtually

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ window.onload = () => {
   createBoard()
   addMines()
   placeClues()
+  renderAllAnswers()
 }
 
 let numOfMines = 50;
@@ -39,7 +40,7 @@ function addMines() {
     while(allCells[random].innerText !== '') {
       random = Math.floor(Math.random() * allCells.length)
     }
-    allCells[random].innerText = 'm'
+    memory[random] = 'm';
   }
 }
 
@@ -84,170 +85,166 @@ function placeCluesOld() {
 }
 
 function placeClues() {
-  let allCells = document.querySelectorAll('.cell')
   for(let cell = 0; cell < (bWidth * bHeight); cell++) {
-    console.log('!!!!!!!!!!!!!!!!!!new cell')
     let counter = 0
-    if(allCells[cell].innerText !== 'm') {
+    if(memory[cell] !== 'm') {
       // not one of the corners
       if(cell !== 0 && cell !== (bWidth - 1) && cell !== ((bHeight-1) * bHeight) && cell !== ((bWidth * bHeight) - 1)) {
         // not one of the edges
         if(cell > bWidth && cell%30 !== 0 && cell%30 !== 29 && cell < ((bWidth * bHeight) - bWidth)) {
-          if(allCells[cell - bWidth - 1].innerText === 'm') {
+          if(memory[cell - bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth].innerText === 'm') {
+          if(memory[cell - bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth + 1].innerText === 'm') {
+          if(memory[cell - bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + 1].innerText === 'm') {
+          if(memory[cell + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth + 1].innerText === 'm') {
+          if(memory[cell + bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth].innerText === 'm') {
+          if(memory[cell + bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth - 1].innerText === 'm') {
+          if(memory[cell + bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - 1].innerText === 'm') {
+          if(memory[cell - 1] === 'm') {
             counter++
           }
-          console.log(`notcorner notedge cell${cell} - counter${counter}`)
           // top row
         } else if(cell < bWidth) {
-          if(allCells[cell + 1].innerText === 'm') {
+          if(memory[cell + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth + 1].innerText === 'm') {
+          if(memory[cell + bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth].innerText === 'm') {
+          if(memory[cell + bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth - 1].innerText === 'm') {
+          if(memory[cell + bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - 1].innerText === 'm') {
+          if(memory[cell - 1] === 'm') {
             counter++
           }
-          console.log(`toprow cell${cell} - counter${counter}`)
           // right side
         } else if(cell%30 === 29) {
-          if(allCells[cell - bWidth - 1].innerText === 'm') {
+          if(memory[cell - bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth].innerText === 'm') {
+          if(memory[cell - bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth].innerText === 'm') {
+          if(memory[cell + bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth - 1].innerText === 'm') {
+          if(memory[cell + bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - 1].innerText === 'm') {
+          if(memory[cell - 1] === 'm') {
             counter++
           }
-          console.log(`rightside cell${cell} - counter${counter}`)
           // bottom row
         } else if(cell >= ((bWidth * bHeight) - bWidth)) {
-          if(allCells[cell - bWidth - 1].innerText === 'm') {
+          if(memory[cell - bWidth - 1] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth].innerText === 'm') {
+          if(memory[cell - bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth + 1].innerText === 'm') {
+          if(memory[cell - bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + 1].innerText === 'm') {
+          if(memory[cell + 1] === 'm') {
             counter++
           }
-          if(allCells[cell - 1].innerText === 'm') {
+          if(memory[cell - 1] === 'm') {
             counter++
           }
-          console.log(`bottomrow cell${cell} - counter${counter}`)
           // left side
         } else if(cell%30 === 0) {
-          if(allCells[cell - bWidth].innerText === 'm') {
+          if(memory[cell - bWidth] === 'm') {
             counter++
           }
-          if(allCells[cell - bWidth + 1].innerText === 'm') {
+          if(memory[cell - bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + 1].innerText === 'm') {
+          if(memory[cell + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth + 1].innerText === 'm') {
+          if(memory[cell + bWidth + 1] === 'm') {
             counter++
           }
-          if(allCells[cell + bWidth].innerText === 'm') {
+          if(memory[cell + bWidth] === 'm') {
             counter++
           }
-        console.log(`leftside cell${cell} - counter${counter}`)
         }
         // top left corner
       } else if(cell === 0) {
-        if(allCells[cell + 1].innerText === 'm') {
+        if(memory[cell + 1] === 'm') {
           counter++
         }
-        if(allCells[cell + bWidth + 1].innerText === 'm') {
+        if(memory[cell + bWidth + 1] === 'm') {
           counter++
         }
-        if(allCells[cell + bWidth].innerText === 'm') {
+        if(memory[cell + bWidth] === 'm') {
           counter++
         }
-        console.log(`topleft cell${cell} - counter${counter}`)
         // top right corner
       } else if(cell === (bWidth - 1)) {
-        if(allCells[cell + bWidth].innerText === 'm') {
+        if(memory[cell + bWidth] === 'm') {
           counter++
         }
-        if(allCells[cell + bWidth - 1].innerText === 'm') {
+        if(memory[cell + bWidth - 1] === 'm') {
           counter++
         }
-        if(allCells[cell - 1].innerText === 'm') {
+        if(memory[cell - 1] === 'm') {
           counter++
         }
-        console.log(`topright cell${cell} - counter${counter}`)
         // bottom right corner
       } else if(cell === ((bWidth * bHeight) - 1)) {
-        if(allCells[cell - bWidth - 1].innerText === 'm') {
+        if(memory[cell - bWidth - 1] === 'm') {
           counter++
         }
-        if(allCells[cell - bWidth].innerText === 'm') {
+        if(memory[cell - bWidth] === 'm') {
           counter++
         }
-        if(allCells[cell - 1].innerText === 'm') {
+        if(memory[cell - 1] === 'm') {
           counter++
         }
-        console.log(`bottomright cell${cell} - counter${counter}`)
         // bottom left corner
       } else if(cell === ((bHeight - 1) * bWidth)) {
-        if(allCells[cell - bWidth].innerText === 'm') {
+        if(memory[cell - bWidth] === 'm') {
           counter++
         }
-        if(allCells[cell - bWidth + 1].innerText === 'm') {
+        if(memory[cell - bWidth + 1] === 'm') {
           counter++
         }
-        if(allCells[cell + 1].innerText === 'm') {
+        if(memory[cell + 1] === 'm') {
           counter++
         }
-        console.log(`bottomleft cell${cell} - counter${counter}`)
       }
     }
     if(counter > 0) {
-      allCells[cell].innerText = counter
+      memory[cell] = counter
     }
   }
 }
 
-
+function renderAllAnswers() {
+  let allCells = document.querySelectorAll('.cell');
+  allCells.forEach((cell, index) => {
+    if(typeof memory[index] !== 'undefined') {
+      cell.innerText = memory[index];
+    }
+  })
+}
 
 
 
